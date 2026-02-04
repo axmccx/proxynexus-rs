257c650c67091649149312d13f3f1516913e779f
Alex McCulloch <alexmccx@gmail.com>
2026-02-03 21:14:24 -0500

    add arg parsing

--- proxynexus-cli/src/main.rs
+++ proxynexus-cli/src/main.rs
@@ -1,11 +1,26 @@
 use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    image_path: String,
+
+    #[arg(short, long, default_value_t = String::from("output.pdf"))]
+    output_path: String,
+}
 
 fn main() {
-    let image_path = PathBuf::from("images/sample.jpg");
-    let output_path = PathBuf::from("output.pdf");
+    let args = Args::parse();
+
+    let image_path = PathBuf::from(&args.image_path);
+    let output_path = PathBuf::from(&args.output_path);
+
+    println!("Adding {} to {}!", args.image_path, args.output_path);
 
     match proxynexus_core::create_pdf_with_image(&image_path, &output_path) {
-        Ok(_) => println!("PDF created successfully: output.pdf"),
+        Ok(_) => println!("PDF created successfully: {}", &args.output_path),
         Err(e) => eprintln!("Error: {}", e),
     }
 }
